5c51c73939b1817151cb04f5b5e0b793d243eac5
Tuan Dat <datng28092005@gmail.com>
2026-01-08 04:52:25 +0700

    Test

---
+++ index.php
@@ -0,0 +1,15 @@
+<form action="lab1.php" method="post">
+    <h3>Mã sinh viên: </h3>
+    <input type="text" name="masv" id="">
+    <h3>Họ tên: </h3>
+    <input type="text" name="hoten" id="">
+    <h3>Địa chỉ: </h3>
+    <input type="text" name="diachi" id="">
+    <h3>Giới tính: </h3>
+    <input type="text" name="GioiTinh" id="">
+    <h3>Quốc gia</h3>
+    <input type="text" name="QuocGia" id="">
+    <h3>Tuổi</h3>
+    <input type="text" name="Tuoi" id="">
+    <input type="submit" name="ok" value="OK">
+</form>
